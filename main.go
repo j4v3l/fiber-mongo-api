205cb1719fcc342a7567f84c3e758c0ca63fb7a6
j4v3l <37094748+j4v3l@users.noreply.github.com>
2022-02-09 20:06:22 -0500

    Routes Setup

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fiber-mongo-api/configs"
 	"log"
-
+    "fiber-mongo-api/routes"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -12,6 +12,9 @@ func main() {
 
     //run database connection
     configs.ConnectDB()
+
+    //run routes
+    routes.UserRoute(app)
 
     log.Fatal(app.Listen(":3000"))
 }
